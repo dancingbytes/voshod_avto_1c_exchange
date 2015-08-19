540bb2c83dacbeff0057a549b3c98d6e6a3a68a4
Tyralion <piliaiev@gmail.com>
2015-08-19 10:02:27 +0500

    Рефакторинг

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 # encoding: utf-8
 VoshodAvtoExchange::Engine.routes.draw do
 
-  # Exchange
-  namespace :exchange do
-
-    get  'init'     => 'base#init'
-
-    get  'users'    => 'base#init'
-    post 'users'    => 'users#list'
-
-    get  'orders'   => 'base#init'
-    post 'orders'   => 'orders#list'
-
-  end # exchange
+  get 'exchange' => 'exchange#init'
 
 end # draw
