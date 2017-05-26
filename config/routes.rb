d028fdca022e8aeb089b95d9f92d52f628800da7
Tyralion <piliaiev@gmail.com>
2017-05-26 10:53:20 +0500

    Мелкие правки

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 VoshodAvtoExchange::Engine.routes.draw do
 
-  get   'exchange' => 'exchange#get'
-  post  'exchange' => 'exchange#post'
+  get   'exchange' => 'exchange#get',   format: false
+  post  'exchange' => 'exchange#post',  format: false
 
 end # draw
